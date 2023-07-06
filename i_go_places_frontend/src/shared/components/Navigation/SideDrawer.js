1941c4bb6faf1287044ffc66d4cda9829518d521
Ritika Mohanty <67157042+basicano@users.noreply.github.com>
2023-07-07 02:49:37 +0530

    Update SideDrawer.js

--- i_go_places_frontend/src/shared/components/Navigation/SideDrawer.js
+++ i_go_places_frontend/src/shared/components/Navigation/SideDrawer.js
@@ -4,8 +4,9 @@ import { CSSTransition } from 'react-transition-group';
 
 
 import './SideDrawer.css';
-
+// represents a side drawer that slides in from the left
 const SideDrawer = props => {
+    // represents the content of the side drawer and is wrapped in a CSSTransition component.
     const content = (
         <CSSTransition
           in={props.show}
@@ -13,7 +14,7 @@ const SideDrawer = props => {
           classNames="slide-in-left"
           mountOnEnter
           unmountOnExit>
-          <aside className="side-drawer" onClick={props.onClick}>{props.children}</aside>
+          <aside className="side-drawer" onClick={props.onClick}>{props.children}</aside>  //The content that should be rendered inside the side drawer.
         </CSSTransition>
       );
 
