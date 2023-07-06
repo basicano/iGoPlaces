687d8e10bbb839f291dd64dc63ce0e3075aec882
Ritika Mohanty <67157042+basicano@users.noreply.github.com>
2023-07-07 01:57:48 +0530

    Update auth-hook.js

--- i_go_places_frontend/src/shared/hooks/auth-hook.js
+++ i_go_places_frontend/src/shared/hooks/auth-hook.js
@@ -2,11 +2,18 @@ import { useState, useCallback, useEffect } from 'react';
 
 let logoutTimer;
 
+// initializes state variables using the useState hook.
 export const useAuth = () => {
-  const [token, setToken] = useState(false);
+  const [token, setToken] = useState(false);          // the authentication token
   const [tokenExpirationDate, setTokenExpirationDate] = useState();
   const [userId, setUserId] = useState(false);
 
+  // memoized callback function created using the useCallback hook. 
+  // It takes the user ID (uid), authentication token, and optional expiration date as parameters. 
+  // It updates the token, userId, and tokenExpirationDate states with the provided values. 
+  // If an expiration date is not provided, it sets a default expiration date one hour from the current time. 
+  // It also saves the user data in the browser's local storage using the localStorage.setItem method.
+  
   const login = useCallback((uid, token, expirationDate) => {
     setToken(token);
     setUserId(uid);
@@ -23,6 +30,8 @@ export const useAuth = () => {
     );
   }, []);
 
+  // memoized callback function created using the useCallback hook. 
+  // It clears the token, tokenExpirationDate, and userId states, and removes the user data from the local storage.
   const logout = useCallback(() => {
     setToken(null);
     setTokenExpirationDate(null);
@@ -30,16 +39,20 @@ export const useAuth = () => {
     localStorage.removeItem('userData');
   }, []);
 
+  // useEffect hook is used to handle the token expiration and automatically log out the user when the token expires
   useEffect(() => {
-    if (token && tokenExpirationDate) {
+    if (token && tokenExpirationDate) {  //  watches for changes in the token, tokenExpirationDate, and logout dependencies
       const remainingTime = tokenExpirationDate.getTime() - new Date().getTime();
+      // calculates the remaining time until the token expires and sets a timer using setTimeout
       logoutTimer = setTimeout(logout, remainingTime);
     } else {
       clearTimeout(logoutTimer);
     }
   }, [token, logout, tokenExpirationDate]);
 
+  // useEffect hook is used to automatically log in the user when the component mounts or when the login function changes. 
   useEffect(() => {
+    // retrieves the user data from the local storage using localStorage.getItem and parses it to an object.
     const storedData = JSON.parse(localStorage.getItem('userData'));
     if (
       storedData &&
@@ -50,5 +63,6 @@ export const useAuth = () => {
     }
   }, [login]);
 
+  // hook returns an object containing the token, login, logout, and userId. These values can be used in any component that uses this hook to manage authentication.
   return { token, login, logout, userId };
 };
